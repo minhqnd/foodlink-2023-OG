2010244f261762b8e4648bbe77502906e8081066
stromez <zemorts@gmail.com>
2023-04-13 09:42:46 +0700

    error

--- dashboard/script/checklogin.js
+++ dashboard/script/checklogin.js
@@ -1,6 +1,6 @@
-var sessionId = document.cookie.match(/sessionId=([^;]+)/);
-if (!sessionId || (new Date().getTime() - new Date(sessionId[1]).getTime()) > 86400000) {
-    window.location.href = "./login.html";
-} else {
-    window.location.href = "./menu.html";
-}
+// var sessionId = document.cookie.match(/sessionId=([^;]+)/);
+// if (!sessionId || (new Date().getTime() - new Date(sessionId[1]).getTime()) > 86400000) {
+//     window.location.href = "./login.html";
+// } else {
+//     window.location.href = "./menu.html";
+// }
