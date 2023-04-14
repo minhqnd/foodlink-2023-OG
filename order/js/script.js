e60fd47eeabcafadc86ec7a3e4e9095ee31f96e5
stromez <zemorts@gmail.com>
2023-04-15 03:24:51 +0700

    update tien

--- order/js/script.js
+++ order/js/script.js
@@ -100,19 +100,78 @@ function render(data, id) {
     $('.listDoAn').append(div)
 }
 
-var danhsach = []
+var productList = []
 
 
 function add(data) {
-    // console.log($('.cart-btn .tien').text());
-    var tien = $('.cart-btn .tien').text() * 1 + $(data).data("gia") * 1 + '.000'
-    $('.tien').text(tien)
-    danhsach.push($(data).data("tensanpham"))
-    var div = listcardtemp
-    var div = div.replaceAll('%ANH%', $(data).data("anh"))
-    var div = div.replaceAll('%GIA%', $(data).data("gia"))
-    var div = div.replaceAll('%TENSANPHAM%', $(data).data("tensanpham"))
-    $('.listcard').append(div)
+    // Lấy thông tin sản phẩm từ thuộc tính data của nút "Thêm"
+    var id = $(data).data('id');
+    var name = $(data).data('tensanpham');
+    var image = $(data).data('anh');
+    var price = $(data).data('gia');
+
+    // Kiểm tra sản phẩm đã tồn tại trong danh sách chưa
+    var product = getProductById(id);
+    if (product !== null) {
+        // Nếu sản phẩm đã tồn tại, tăng số lượng và cập nhật tổng tiền
+        product.quantity++;
+        product.totalPrice = product.quantity * price;
+    } else {
+        // Nếu sản phẩm chưa tồn tại, thêm vào danh sách
+        var product = {
+            id: id,
+            name: name,
+            image: image,
+            price: price * 1,
+            quantity: 1,
+            totalPrice: price * 1
+        };
+        productList.push(product);
+    }
+    updateCart(productList)
+}
+
+// Hàm tìm kiếm sản phẩm trong danh sách theo ID
+function getProductById(id) {
+    for (var i = 0; i < productList.length; i++) {
+        if (productList[i].id === id) {
+            return productList[i];
+        }
+    }
+    return null;
+}
+
+function tongTien(productList) {
+    var moneys = 0
+    productList.forEach(data => {
+        moneys = moneys + data.totalPrice
+    })
+    return moneys
+}
+
+function tongDon(productList) {
+    var dons = 0
+    productList.forEach(data => {
+        dons = dons + data.quantity
+    })
+    return dons
+}
+
+function updateCart(productList) {
+    $('.listcard').empty()
+    $('.tien').text(tongTien(productList) + '.000')
+    $('.badge').text(tongDon(productList))
+    productList.forEach(data => {
+        var div = listcardtemp
+        var div = div.replaceAll('%ANH%', data.image)
+        var div = div.replaceAll('%ID%', data.id)
+        var div = div.replaceAll('%TOTALPRICE%', data.totalPrice + '.000')
+        var div = div.replaceAll('%PRICE%', data.price)
+        var div = div.replaceAll('%SOLUONG%', data.quantity)
+        var div = div.replaceAll('%TENSANPHAM%', data.name)
+        $('.listcard').append(div)
+    });
+
 }
 
 var listcardtemp = `<div class="box">
@@ -120,14 +179,41 @@ var listcardtemp = `<div class="box">
 <img src="%ANH%" loading="lazy" alt="">
 <div class="content">
     <h3>%TENSANPHAM%</h3>
-    <span>Số lượng : </span>
-    <input type="number" name="" value="1" id="" onChange="console.log('cc')">
+    <span>Số lượng: </span>
+    <input data-id='%ID%' data-price='%PRICE%' type="number" name="" value="%SOLUONG%" id="" min=1 max=10 onChange="themSoLuong(this)">
     <br>
-    <span>Giá : </span>
-    <span class="price"> %GIA% </span>
+    <span>Giá: </span>
+    <span class="price"> %TOTALPRICE% </span>
 </div>
 </div>`
 
+function themSoLuong(input) {
+    // Lấy giá trị của input
+    var soLuong = input.value;
+
+    // Lấy ID sản phẩm
+    var id = $(input).data("id");
+
+    // Lấy giá sản phẩm
+    var price = $(input).data("price");
+
+    // Tính toán tổng giá mới)
+    var total = soLuong * price;
+
+    // Tìm sản phẩm tương ứng trong productList và cập nhật số lượng và tổng giá mới
+    for (var i = 0; i < productList.length; i++) {
+        if (productList[i].id == id) {
+            productList[i].soLuong = soLuong;
+            productList[i].totalPrice = total;
+
+            // Cập nhật lại giá trị của sản phẩm trên trang
+            $(input).siblings(".price").text(total + '.000');
+            $('.tien').text(tongTien(productList) + '.000')
+            $('.badge').text(tongDon(productList))
+            break;
+        }
+    }
+}
 
 
 
@@ -139,7 +225,7 @@ function updonhang() {
     // }
     // checkcan(id)
     var id = Math.floor(Math.random() * 90 + 10)
-    
+
     firebase.database().ref(`donhang/${id}`).update({
         danhsach: danhsach,
         gia: $('.cart-btn .tien').text(),
