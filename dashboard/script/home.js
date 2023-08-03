4ab55b220567c5276db3764e39160e80279c166a
Quang Minh <zemorts@gmail.com>
2023-08-03 07:19:59 +0700

    roll back

--- dashboard/script/home.js
+++ dashboard/script/home.js
@@ -7,3 +7,23 @@ $.getJSON(`https://order-app-moi-default-rtdb.asia-southeast1.firebasedatabase.a
 });
 
 console.log('cccc')
+
+firebase.database().ref("donhientai").on("change", snapshot => {
+    // console.log(snapshot.val());
+    // if (!snapshot.val().trangthai) {
+    //     snapshot.val().danhsach.forEach(element => {
+    //         cart.push(element)
+    //     });
+    //     var div = order
+    //     var div = div.replaceAll('%DANHSACH%', snapshot.val().danhsach.join("<br>"))
+    //     var div = div.replaceAll('%ID%', snapshot.val().id)
+    //     var div = div.replaceAll('%IDKEY%', snapshot.key)
+    //     var div = div.replaceAll('%TIME%', snapshot.val().time)
+    //     var div = div.replaceAll('%GIA%', snapshot.val().gia + '₫')
+    //     $('.listOrder').prepend(div)
+    // }
+    // $('.tonghop').empty()
+    // $('.tonghop').text(consolidateOrders(cart))
+    console.log('cccccccc')
+    console.log(snapshot.val())
+})
